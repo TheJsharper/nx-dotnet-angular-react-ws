c6d31613b1dca456ae6de66f59a79d62ca9dd74f
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-07-12 16:16:13 +0200

    Market simple event implementation

---
+++ backend/dotnet/libs/beyondPattern.Test/DesignPattern/Observable/ViaCollections/MarketTest.cs
@@ -0,0 +1,35 @@
+﻿using Backend.Dotnet.Libs.Beyond.Pattern.DesignPatterns.Observable.ViaCollections;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Dotnet.Libs.Beyond.Pattern.Test.DesignPattern.Observable.ViaCollections
+{
+    public class MarketTest
+    {
+
+
+        [Fact]
+        public void ValitilityTest()
+        {
+
+            var market = new Market();
+            market.PropertyChanged += Market_PropertyChanged;
+            market.Valitility = 123.01f;
+            Assert.Equal(123.01f, market.Valitility);
+        }
+
+        private void Market_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+           if(e.PropertyName == "Valitility")
+            {
+                Console.WriteLine($"Called  Valitility {e.ToString()}");
+            } 
+        }
+
+      
+    }
+}
---
+++ backend/dotnet/libs/beyondPattern/DesignPatterns/Observable/ViaCollections/Market.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Dotnet.Libs.Beyond.Pattern.DesignPatterns.Observable.ViaCollections
+{
+    public class Market:INotifyPropertyChanged
+    {
+        private float valitility;
+
+        public float Valitility { get => valitility; set {
+                if (value.Equals(valitility)) return;
+                valitility = value;
+                OnPropertyChanged(nameof(Valitility));
+            } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            if(!propertyName.Equals(""))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+}
