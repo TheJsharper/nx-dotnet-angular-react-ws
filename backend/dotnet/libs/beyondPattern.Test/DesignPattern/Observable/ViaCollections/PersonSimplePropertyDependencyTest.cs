43507be66156c8bd06fad28c0778b6e12774df89
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-07-17 15:44:08 +0200

    Simple dependency properties

---
+++ backend/dotnet/libs/beyondPattern.Test/DesignPattern/Observable/ViaCollections/PersonSimplePropertyDependencyTest.cs
@@ -0,0 +1,57 @@
+﻿using Backend.Dotnet.Libs.Beyond.Pattern.DesignPatterns.Observable.ViaCollections;
+
+namespace Backend.Dotnet.Libs.Beyond.Pattern.Test.DesignPattern.Observable.ViaCollections
+{
+    public class PersonSimplePropertyDependencyTest
+    {
+
+        [Fact]
+
+        public void PropertyDependencyIfCanVoteFalseTest()
+        {
+            var person = new PersonSimplePropertyDependency();
+
+            person.PropertyChanged += (sender, eventArgs)=> {
+                string? propertyName = eventArgs.PropertyName;
+
+                Assert.Equal("Age", propertyName);
+            
+            };
+            person.Age = 15;
+
+            Assert.False(person.CanVote);
+        }
+
+        [Fact]
+        public void PropertyDependencyIfCanVoteTrueTest()
+        {
+            var person = new PersonSimplePropertyDependency();
+            
+            var value = 16;
+            
+            Assert.PropertyChanged(person, "Age", ()=>  person.Age = value);
+            
+
+            Assert.True(person.CanVote);
+
+            Assert.Equal(value, person.Age);
+        }
+
+        [Fact]
+        public void PropertyDependencyIfCanVoteEventRaiseTest()
+        {
+            var person = new PersonSimplePropertyDependency();
+
+            var value = 16;
+
+            Assert.PropertyChanged(person, "CanVote", () => person.Age = value);
+
+
+            Assert.True(person.CanVote);
+
+            Assert.Equal(value, person.Age);
+        }
+
+
+    }
+}
---
+++ backend/dotnet/libs/beyondPattern/DesignPatterns/Observable/ViaCollections/PersonSimplePropertyDependency.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Backend.Dotnet.Libs.Beyond.Pattern.DesignPatterns.Observable.ViaCollections
+{
+    public class PersonSimplePropertyDependency:INotifyPropertyChanged
+    {
+        private int age;
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+
+                var oldCanVote = CanVote;
+
+                if (value == age) return;
+                age = value;
+                OnPropertyChanged();
+
+                if(oldCanVote != CanVote)                
+                    OnPropertyChanged(nameof(CanVote));               
+
+            }
+        }
+
+        public bool CanVote => Age >= 16;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
