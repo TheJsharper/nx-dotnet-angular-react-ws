97be078507a49e280dcb129e21036d9892f4aa03
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-08-31 23:50:27 +0200

    testing service

--- frontend/angular/apps/gifs-app/src/app/app.module.ts
+++ frontend/angular/apps/gifs-app/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
-
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,7 +11,8 @@ import { SharedModule } from './shared/shared.module';
     //RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
 
     GifsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
 
   ],
   providers: [],
--- frontend/angular/apps/gifs-app/src/app/gifs/services/gifs.service.ts
+++ frontend/angular/apps/gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,49 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 
 export class GifsService {
 
     private _tagsHistory: string[] = [];
 
-    constructor() { }
+    private API_KEY:string ="DlyhUudrpjVFrMFoZ0y7GISYkLk0ABt5";
+
+    private BASE_URL:string= 'http://api.giphy.com/v1/gifs';
+
+    constructor(private httpClient: HttpClient) { }
 
     get TagsHistory(): string[] {
         return [...this._tagsHistory];
     }
 
-    searchTag(tag: string): void {
+    private organizeHistory(tag: string): void {
+        tag = tag.toLowerCase();
+        if (this._tagsHistory.includes(tag)) {
+            this._tagsHistory = this._tagsHistory.filter((olTag) => olTag !== tag);
+        }
         this._tagsHistory.unshift(tag);
+        this._tagsHistory = this.TagsHistory.splice(0,10);
+    }
+
+    searchTag(tag: string): void {
+        if (tag.length === 0) return;
+        
+        this.organizeHistory(tag);
+
+        const params = new HttpParams();
+
+        params.set('api_key', this.API_KEY);
+        params.set('limit', 10);
+        params.set('q', tag);
+        this.httpClient.get(`${this.BASE_URL}/search`, {params, responseType:'json'})
+        .subscribe(console.log)
+        /*fetch('http://api.giphy.com/v1/gifs/search?q=ryan gosling&api_key=DlyhUudrpjVFrMFoZ0y7GISYkLk0ABt5&limnit=10')
+        .then(resp=> resp.json())
+        .then(console.log)*/
+
+       // this._tagsHistory.unshift(tag);
     }
 
 }
