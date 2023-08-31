4bab362dfcb377aa1c995193c03a321226ca3129
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-08-31 17:53:58 +0200

    layout quite app csss

--- frontend/angular/apps/gifs-app/src/app/app.module.ts
+++ frontend/angular/apps/gifs-app/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    //RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+
+    GifsModule,
+    SharedModule
+
   ],
   providers: [],
   bootstrap: [AppComponent],
