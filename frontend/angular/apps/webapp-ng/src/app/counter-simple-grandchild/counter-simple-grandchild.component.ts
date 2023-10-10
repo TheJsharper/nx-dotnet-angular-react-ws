2f300496d5012ac74c6ad45d04ab7a98a1134420
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-10-10 21:45:12 +0200

    input output resolved problems

--- frontend/angular/apps/webapp-ng/src/app/counter-simple-grandchild/counter-simple-grandchild.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-counter-simple-grandchild',
-  templateUrl: './counter-simple-grandchild.component.html',
-  styleUrls: ['./counter-simple-grandchild.component.scss'],
-})
-export class CounterSimpleGrandchildComponent {}
--- frontend/angular/apps/webapp-ng/src/app/counter-simple/counter-simple.module.ts
+++ frontend/angular/apps/webapp-ng/src/app/counter-simple/counter-simple.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { CounterSimpleComponent } from './counter-simple.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { CounterSimpleChildComponent } from '../counter-simple-child/counter-simple-child.component';
-import { CounterSimpleGrandchildComponent } from '../counter-simple-grandchild/counter-simple-grandchild.component';
+import { CounterSimpleChildComponent } from './counter-simple-child/counter-simple-child.component';
+import { CounterSimpleGrandchildComponent } from './counter-simple-grandchild/counter-simple-grandchild.component';
 
 export const routes: Routes = [{
   path: '',
