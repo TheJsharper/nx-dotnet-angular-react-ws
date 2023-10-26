0eba218688cf1cd8e938880fbf35ec49355d5c41
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-10-26 21:36:46 +0200

    fixed immutable store

--- frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/services/ngrx-create-api-plot.service.ts
+++ frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/services/ngrx-create-api-plot.service.ts
@@ -1,5 +1,6 @@
-import { ElementRef, Injectable, RendererFactory2 } from "@angular/core";
+import { ElementRef, Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
+import { cloneDeep } from 'lodash';
 import { Config, Data, Layout, PlotData, PlotlyHTMLElement, newPlot } from 'plotly.js-dist-min';
 import { Observable, map, of } from "rxjs";
 import { PlotModel } from "../store/ngrx-create-api-plot.models";
@@ -19,17 +20,18 @@ export class NgrxCreateApliPlotService {
         const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
         const yArray = [55, 49, 44, 24, 15];
 
+
         const data: Array<Partial<PlotData>> = [{
             x: xArray,
             y: yArray,
             type: "bar",
             orientation: "v",
 
-            // marker: { color: "rgba(0,0,255)" }
+            marker: { color: "rgba(0, 0, 255, 0.5)" }
         }];
 
         const plotModel: PlotModel = {
-            data, selected: { key: xArray[0], value: yArray[0], index:0 }
+            data, selected: { key: xArray[0], value: yArray[0], index: 0 }
         }
 
         return of(plotModel);
@@ -40,16 +42,14 @@ export class NgrxCreateApliPlotService {
 
 
                 const layout: Partial<Layout> = {
-                    title: 'Responsive to window\'s size!',
                     font: { size: 18 },
 
                 };
 
                 const config: Partial<Config> = { responsive: true }
+                const newData = data.map((value: Partial<Data>) => cloneDeep(value))
 
-
-
-                return await newPlot(parent.nativeElement, data, layout, config);
+                return await newPlot(parent.nativeElement, newData, layout, config);
             }
         ));
         return result;
