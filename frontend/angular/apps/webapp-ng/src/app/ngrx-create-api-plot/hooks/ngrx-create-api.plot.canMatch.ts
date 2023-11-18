3d8f8f5bff474fc9dcf96505f6df6a45d2790538
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-11-18 19:48:08 +0100

    prefixed and removed all unused code

--- frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/hooks/ngrx-create-api.plot.canMatch.ts
+++ frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/hooks/ngrx-create-api.plot.canMatch.ts
@@ -1,14 +1,11 @@
 
 import { inject } from '@angular/core';
-import { CanMatchFn, Route, UrlSegment } from '@angular/router';
+import { CanMatchFn } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { LoadingPlotDataAction } from '../store/ngrx-create-api-plot.actions';
 import { PlotModel } from '../store/ngrx-create-api-plot.models';
 
-export const NgrxCreateApiCanMatchGuard: CanMatchFn = (
-    route: Route,
-    segments: UrlSegment[]
-) => {
+export const NgrxCreateApiCanMatchGuard: CanMatchFn = () => {
     inject(Store<PlotModel>).dispatch(LoadingPlotDataAction())
     return true;
 }
