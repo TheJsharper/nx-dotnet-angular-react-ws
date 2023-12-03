364f063c624b8571a89d62e87d5fd4ba13b1fdd8
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-12-03 23:00:59 +0100

    zomm handling

--- frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/ngrx-create-api-plot-menu-bar/ngrx-create-api-plot-menu-bar.component.ts
+++ frontend/angular/apps/webapp-ng/src/app/ngrx-create-api-plot/ngrx-create-api-plot-menu-bar/ngrx-create-api-plot-menu-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { PlotRelayoutEvent, PlotlyHTMLElement } from "plotly.js-dist-min";
-import { Subject, takeUntil } from "rxjs";
+import { Subject, fromEvent, takeUntil } from "rxjs";
+import { map, mergeMap } from 'rxjs/operators';
 import { NgrxCreateApiPlotMainService } from "../../services/ngrx-create-api-plot-main.service";
 import { NgrxCreateApiPlotZoomService } from "../services/ngrx-create-api-plot-zoom.service";
 
@@ -9,13 +10,19 @@ import { NgrxCreateApiPlotZoomService } from "../services/ngrx-create-api-plot-z
     selector: 'app-ngrx-create-api-plot-menu-bar',
     templateUrl: './ngrx-create-api-plot-menu-bar.component.html'
 })
-export class NgrxCreateApiMenubarComponent implements OnInit, OnDestroy {
+export class NgrxCreateApiMenubarComponent implements OnInit, OnDestroy, AfterViewInit {
 
     form: FormGroup;
+
+    @ViewChild('zoomIn') zoomIn?: ElementRef;
+
+    @ViewChild('zoomOut') zoomOut?: ElementRef;
 
     private plotInstance: Promise<PlotlyHTMLElement>;
 
     private signalDestroyer$: Subject<void>;
+
+
 
     constructor(
         private ngrxCreateApiPlotZoomService: NgrxCreateApiPlotZoomService,
@@ -31,9 +38,17 @@ export class NgrxCreateApiMenubarComponent implements OnInit, OnDestroy {
             axe: new FormControl<'xaxe' | 'yaxe'>('xaxe', { nonNullable: true }),
         });
 
-        this.form.valueChanges.subscribe(console.log)
-
         this.form.get('axe')?.valueChanges.subscribe(console.log)
+    }
+    ngAfterViewInit(): void {
+        const control = this.form.get<string>('axe')?.valueChanges;
+        const zIn =fromEvent<PointerEvent>(<HTMLButtonElement>this.zoomIn?.nativeElement, 'click').pipe(map((value:PointerEvent)=> value));
+
+        control?.pipe(mergeMap((axe:'xaxe' | 'yaxe')=> zIn.pipe(map(( )=> axe)))).subscribe((value)=> console.log("Clicked", value));
+        
+        
+      //  fromEvent(<HTMLButtonElement>this.zoomOut?.nativeElement, 'click').subscribe((value) => console.log("CLICKKKKK", value));
+
     }
 
 
