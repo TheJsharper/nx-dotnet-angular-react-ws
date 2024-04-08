f464d75f83b3791c01e2e4e6386af02864410c35
Pedro Salomon <pedro.salomon@gmail.com>
2024-04-08 20:52:12 +0200

    reset error

--- frontend/react/apps/one-webapp/src/app/components/without-query/without-query.component.tsx
+++ frontend/react/apps/one-webapp/src/app/components/without-query/without-query.component.tsx
@@ -2,7 +2,7 @@ import { useEffect, useReducer, useState } from "react"
 
 const getRandomNumberFromApi = async (): Promise<number> => {
     const random_number: number = await new Promise(resolve => setTimeout(() => resolve(Math.floor(Math.random() * 1000) + 1), 1000));
-    //if (random_number > 500) throw new Error('upsi!')
+    if (random_number > 500) throw new Error('upsi!')
     return +random_number;
 }
 
@@ -17,7 +17,7 @@ export const WithoutQuery: () => JSX.Element = () => {
 
     useEffect(() => {
         setIsloading(true);
-        getRandomNumberFromApi().then(nun => setCount(nun)).catch(error => setError(error.message))
+        getRandomNumberFromApi().then(nun => {setCount(nun); setError(undefined)}).catch(error => setError(error.message))
     }, [key]);
 
     useEffect(() => {
