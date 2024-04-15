cfc224c736aa18b56fb80a112474ad937e765875
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-04-15 15:51:13 +0200

    added building rest api with spring boot

---
+++ backend/spring/apps/building-rest-api-with-sb/src/test/java/com/spring/academy/BuildingRestApiWithSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestApiWithSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
