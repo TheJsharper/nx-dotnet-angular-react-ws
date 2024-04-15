72109c50a814184cc605ecf5b025bae57dc5ce01
Pedro Salomon <pedro.salomon@gmail.com>
2024-04-15 22:07:36 +0200

    fixed problem loading resources

--- backend/spring/apps/building-rest-api-with-sb/src/test/java/com/spring/academy/CashCardJsonTest.java
+++ backend/spring/apps/building-rest-api-with-sb/src/test/java/com/spring/academy/CashCardJsonTest.java
@@ -2,17 +2,13 @@ package com.spring.academy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-
-import com.jayway.jsonpath.internal.Path;
 
 @JsonTest
 public class CashCardJsonTest {
@@ -27,24 +23,8 @@ public class CashCardJsonTest {
     @Test
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
-        java.nio.file.Path resourceDirectory = Paths.get("src","test","resources"); //Paths.get("src", "test","java", "com", "spring", "academy", "resources");
-        String absolutePath = resourceDirectory.toFile().getAbsolutePath();;
-        resourceDirectory.forEach((p)->{
-            System.out.println("===>"+p.getFileName());
-        } );
-        System.out.println(absolutePath);
 
-   /*      ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("expected.json").getFile());
-        System.out.println(file.getAbsolutePath());
- */
-
-        InputStream is = getClass().getClassLoader().getSystemResourceAsStream("expected.json"); //.getResourceAsStream(absolutePath+"/expected.json");
-
-        /*
-         * =
-         * getClass().getClassLoader().getResourceAsStream("resources/expected.json");
-         */
+        InputStream is = getClass().getClassLoader().getResourceAsStream("expected.json");         
         assertThat(json.write(cashCard)).isStrictlyEqualToJson(is);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
