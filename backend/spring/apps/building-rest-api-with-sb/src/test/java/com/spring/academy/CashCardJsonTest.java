bff158a9a8f426b75b5d65ddc9e8cf2b2ef5a9d7
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-04-16 09:37:06 +0200

    fixed file reading

--- backend/spring/apps/building-rest-api-with-sb/src/test/java/com/spring/academy/CashCardJsonTest.java
+++ backend/spring/apps/building-rest-api-with-sb/src/test/java/com/spring/academy/CashCardJsonTest.java
@@ -2,8 +2,11 @@ package com.spring.academy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +26,9 @@ public class CashCardJsonTest {
     @Test
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
-
-        InputStream is = getClass().getClassLoader().getResourceAsStream("expected.json");         
+        File file = new File("./src/test/resources/expected.json");
+       // Arrays.asList(file.list()).forEach((name)-> System.out.println("name ===> "+name));
+        FileInputStream is =  new FileInputStream(file);// getClass().getClassLoader().getResourceAsStream("expected.json");         
         assertThat(json.write(cashCard)).isStrictlyEqualToJson(is);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
