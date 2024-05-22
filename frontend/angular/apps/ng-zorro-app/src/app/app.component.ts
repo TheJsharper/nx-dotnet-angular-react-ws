4b0f50e96e6c86f69a1f5e7986c5f5ba7bcb689a
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-05-22 17:04:27 +0200

    provideAnimations AppConfig

--- frontend/angular/apps/ng-zorro-app/src/app/app.component.ts
+++ frontend/angular/apps/ng-zorro-app/src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { AccountBookFill, AlertFill, AlertOutline, MenuFoldOutline, MenuUnfoldOutline, TeamOutline, UserOutline } from '@ant-design/icons-angular/icons';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NgZorroTableComponent } from "ng-zorro-table";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+let icons: IconDefinition[] = [AccountBookFill, AlertOutline, AlertFill, MenuFoldOutline, MenuUnfoldOutline, TeamOutline, UserOutline];
+@NgModule({
+  exports: [NzIconModule],
+  imports: [NzIconModule.forChild(icons), CommonModule]
+})
+export class IConModule { }
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule,  NzButtonModule],
+  imports: [
+   /* BrowserAnimationsModule,  BrowserModule, NgIf, NgFor,CommonModule,*/ 
+   RouterModule, NzButtonModule, NgZorroTableComponent, NzLayoutModule, NzSliderModule,
+    NzBreadCrumbModule,
+    IConModule, NzMenuModule
+
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-zorro-app';
+  isCollapsed = false;
 }
+
