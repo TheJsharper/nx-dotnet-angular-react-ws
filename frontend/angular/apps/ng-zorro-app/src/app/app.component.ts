89c8e9d6b5b2cbdc30118ef946c7542cbbb384ed
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-05-22 18:17:12 +0200

    router config added

--- frontend/angular/apps/ng-zorro-app/src/app/app.component.ts
+++ frontend/angular/apps/ng-zorro-app/src/app/app.component.ts
@@ -24,7 +24,6 @@ export class IConModule { }
     RouterModule, NzButtonModule, NgZorroTableComponent, NzLayoutModule, NzSliderModule,
     NzBreadCrumbModule,
     IConModule, NzMenuModule
-
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
--- frontend/angular/apps/ng-zorro-app/src/app/app.routes.ts
+++ frontend/angular/apps/ng-zorro-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+//import { NgZorroTableComponent } from "ng-zorro-table";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+
+    {
+        path:'ng-zorro-table', 
+        loadComponent: ()=> import('ng-zorro-table').then(m => m.NgZorroTableComponent ),
+    },
+    {
+        path: '',
+        redirectTo: 'ng-zorro-table',
+        pathMatch: 'full'
+      }
+];
