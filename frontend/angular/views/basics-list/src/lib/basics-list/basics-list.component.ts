b421ecc2bb4b7808fe99b4f53e3da3fbe7d83ff7
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-05-23 18:41:30 +0200

    list fillout

--- frontend/angular/views/basics-list/src/lib/basics-list/basics-list.component.ts
+++ frontend/angular/views/basics-list/src/lib/basics-list/basics-list.component.ts
@@ -1,14 +1,23 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faker } from '@faker-js/faker';
 import { NzListModule } from "ng-zorro-antd/list";
+export interface Data {
+  descris: string;
+  content: string[];
+  avatar: string;
+}
 @Component({
   selector: 'lib-basics-list',
   standalone: true,
-  imports: [ NgFor, NgIf, NzListModule ],
+  imports: [NgFor, NgIf, NzListModule],
   templateUrl: './basics-list.component.html',
   styleUrl: './basics-list.component.scss',
 })
-export class BasicsListComponent {
+export class BasicsListComponent implements OnInit {
+  ngOnInit(): void {
+    this.generateItem();
+  }
   loading = false;
   data = [
     {
@@ -51,5 +60,17 @@ export class BasicsListComponent {
         this.loading = false;
       }, 1000);
     }
+  }
+
+  private generateItem(): Data[] {
+    console.log("==>", new Array(100).fill(0).map((_) =>{ 
+      console.log("==>",_)
+      return faker.person.bio()}));
+    /* const p =faker.person;
+        faker.image.avatar();
+        faker.definitions.metadata.title;
+        faker.definitions.lorem.words;
+      } */
+    return [];
   }
 }
