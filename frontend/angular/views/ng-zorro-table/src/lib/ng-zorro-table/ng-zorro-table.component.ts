b2635aa5d268277bbc1ec9e0c8af35528c703735
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-05-23 11:36:42 +0200

    table update setInterval

--- frontend/angular/views/ng-zorro-table/src/lib/ng-zorro-table/ng-zorro-table.component.ts
+++ frontend/angular/views/ng-zorro-table/src/lib/ng-zorro-table/ng-zorro-table.component.ts
@@ -1,12 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { faker } from '@faker-js/faker';
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
   selector: 'lib-ng-zorro-table',
   standalone: true,
- // imports: [CommonModule],
+  imports: [CommonModule, NzTableModule, NzDividerModule],
   templateUrl: './ng-zorro-table.component.html',
   styleUrl: './ng-zorro-table.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+ // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NgZorroTableComponent {}
+export class NgZorroTableComponent implements OnInit {
+
+  listOfData: Person[] = [this.createRandomUser()];
+
+  ngOnInit(): void {
+
+   
+
+    setInterval(() => {
+      //this.listOfData.push( this.createRandomUser());
+      //this.listOfData = [...this.listOfData]
+      //this.listOfData = this.listOfData.map((d)=> this.createRandomUser() );
+      this.listOfData = [...this.listOfData, this.createRandomUser()]
+      console.log(this.listOfData)
+    }, 5000);
+  }
+
+  private  createRandomUser(): Person {
+    return {
+      key: faker.string.uuid(),
+      name: faker.person.fullName(),
+      age: Math.floor( Math.random() *90) +1,
+      address : faker.location.country()
+    };
+  }
+  
+}
