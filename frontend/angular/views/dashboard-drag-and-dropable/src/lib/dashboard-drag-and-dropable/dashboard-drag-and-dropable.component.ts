10e297d82c6b6ba7039255db624f144d7a43caf5
Pedro Salomon <pedro.salomon@gmail.com>
2024-05-28 23:29:11 +0200

    testing dashboard with angular-gridster

--- frontend/angular/views/dashboard-drag-and-dropable/src/lib/dashboard-drag-and-dropable/dashboard-drag-and-dropable.component.ts
+++ frontend/angular/views/dashboard-drag-and-dropable/src/lib/dashboard-drag-and-dropable/dashboard-drag-and-dropable.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GridsterComponent, GridsterConfig, GridsterItem, GridsterItemComponent, GridsterItemComponentInterface } from 'angular-gridster2';
-
+import { DisplayGrid, GridType, GridsterComponent, GridsterConfig, GridsterItem, GridsterItemComponent, GridsterItemComponentInterface } from 'angular-gridster2';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu'
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'lib-dashboard-drag-and-dropable',
   standalone: true,
-  imports: [CommonModule, GridsterComponent, GridsterItemComponent],
+  imports: [CommonModule, GridsterComponent, GridsterItemComponent, MatCheckboxModule, MatIconModule, MatMenuModule, FormsModule],
   templateUrl: './dashboard-drag-and-dropable.component.html',
   styleUrl: './dashboard-drag-and-dropable.component.scss',
 })
@@ -13,43 +16,56 @@ export class DashboardDragAndDropableComponent implements OnInit {
 
   options!: GridsterConfig;
   dashboard!: Array<GridsterItem>;
-  constructor() {
 
-
-
-  }
-
-  static itemChange(item: GridsterItem, itemComponent: GridsterItemComponentInterface) {
-    console.info('itemChanged', item, itemComponent);
-  }
-
-  static itemResize(item: GridsterItem, itemComponent: GridsterItemComponentInterface) {
-    console.info('itemResized', item, itemComponent);
-  }
-
-  ngOnInit() {
+  // noinspection DuplicatedCode
+  ngOnInit(): void {
     this.options = {
-      itemChangeCallback: DashboardDragAndDropableComponent.itemChange,
-      itemResizeCallback: DashboardDragAndDropableComponent.itemResize,
+      gridType: GridType.Fit,
+      displayGrid: DisplayGrid.Always,
+      pushItems: false,
+      swap: true,
+      allowMultiLayer: true,
+      defaultLayerIndex: 1,
+      baseLayerIndex: 2,
+      maxLayerIndex: 2,
+      swapWhileDragging: false,
+      draggable: {
+        enabled: true
+      },
+      resizable: {
+        enabled: true
+      }
     };
 
     this.dashboard = [
-      { cols: 2, rows: 10, y: 0, x: 0 },
-      { cols: 2, rows: 20, y: 0, x: 2 }
+      { cols: 2, rows: 1, y: 0, x: 0, layerIndex: 2 },
+      { cols: 2, rows: 2, y: 0, x: 2 },
+      { cols: 1, rows: 1, y: 0, x: 4 },
+      { cols: 3, rows: 2, y: 1, x: 4 },
+      { cols: 1, rows: 1, y: 4, x: 5 },
+      { cols: 1, rows: 1, y: 2, x: 1 },
+      { cols: 2, rows: 2, y: 5, x: 5 },
+      { cols: 2, rows: 2, y: 3, x: 2 },
+      { cols: 2, rows: 1, y: 2, x: 2 },
+      { cols: 1, rows: 1, y: 3, x: 4 },
+      { cols: 1, rows: 1, y: 0, x: 6 }
     ];
   }
 
-  changedOptions() {
-    if (this.options && this.options.api && this.options.api.optionsChanged)
+  changedOptions(): void {
+    if (this.options.api && this.options.api.optionsChanged) {
       this.options.api.optionsChanged();
+    }
   }
 
-  removeItem(item:GridsterItem) {
+  removeItem($event: MouseEvent | TouchEvent, item: GridsterItem): void {
+    $event.preventDefault();
+    $event.stopPropagation();
     this.dashboard.splice(this.dashboard.indexOf(item), 1);
   }
 
-  addItem() {
-   // this.dashboard.push({});
+  addItem(): void {
+    this.dashboard.push({ x: 0, y: 0, cols: 2, rows: 1 });
   }
 
 }
