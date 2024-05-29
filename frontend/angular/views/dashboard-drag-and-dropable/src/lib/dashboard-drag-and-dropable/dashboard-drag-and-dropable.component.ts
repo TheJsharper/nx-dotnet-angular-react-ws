17b42bd8b368b1a20706609bd19a04c8874a477c
Pedro Salomon <pedro.salomon@gmail.com>
2024-05-29 22:58:08 +0200

    permission for admin and user

--- frontend/angular/views/dashboard-drag-and-dropable/src/lib/dashboard-drag-and-dropable/dashboard-drag-and-dropable.component.ts
+++ frontend/angular/views/dashboard-drag-and-dropable/src/lib/dashboard-drag-and-dropable/dashboard-drag-and-dropable.component.ts
@@ -25,6 +25,8 @@ export class DashboardDragAndDropableComponent implements OnInit {
   options!: Safe;
   dashboard!: Array<GridsterItem>;
 
+  permission:"admin"| "user" = "admin";
+
   ngOnInit(): void {
     this.options = {
       gridType: GridType.Fit,
@@ -134,11 +136,124 @@ export class DashboardDragAndDropableComponent implements OnInit {
     }
   }
 
-  changeUserPermission(personmssion:'admin'| 'user'):void{
-    if(personmssion === "admin"){
-      this.options.draggable = {...this.options.draggable, enabled:true}
+  changeUserPermission():void{
+    if(this.permission === "user"){
+      this.options = {
+        gridType: GridType.Fit,
+        compactType: CompactType.None,
+        margin: 10,
+        outerMargin: true,
+        outerMarginTop: null,
+        outerMarginRight: null,
+        outerMarginBottom: null,
+        outerMarginLeft: null,
+        useTransformPositioning: true,
+        mobileBreakpoint: 640,
+        useBodyForBreakpoint: false,
+        minCols: 1,
+        maxCols: 100,
+        minRows: 1,
+        maxRows: 100,
+        maxItemCols: 100,
+        minItemCols: 1,
+        maxItemRows: 100,
+        minItemRows: 1,
+        maxItemArea: 2500,
+        minItemArea: 1,
+        defaultItemCols: 1,
+        defaultItemRows: 1,
+        fixedColWidth: 105,
+        fixedRowHeight: 105,
+        keepFixedHeightInMobile: false,
+        keepFixedWidthInMobile: false,
+        scrollSensitivity: 10,
+        scrollSpeed: 20,
+        enableEmptyCellClick: false,
+        enableEmptyCellContextMenu: false,
+        enableEmptyCellDrop: false,
+        enableEmptyCellDrag: false,
+        enableOccupiedCellDrop: false,
+        emptyCellDragMaxCols: 50,
+        emptyCellDragMaxRows: 50,
+        ignoreMarginInRow: false,
+        draggable: {
+          enabled: false, 
+        },
+        resizable: {
+          enabled: false
+        },
+        swap: false,
+        pushItems: false,
+        disablePushOnDrag: false,
+        disablePushOnResize: false,
+        pushDirections: { north: false, east: false, south: false, west: false },
+        pushResizeItems: false,
+        displayGrid: DisplayGrid.Always,
+        disableWindowResize: false,
+        disableWarnings: false,
+        scrollToNewItems: false
+      };
+      
     }else{
-      this.options.draggable = {...this.options.draggable, enabled:false}
+      this.options = {
+        gridType: GridType.Fit,
+        compactType: CompactType.None,
+        margin: 10,
+        outerMargin: true,
+        outerMarginTop: null,
+        outerMarginRight: null,
+        outerMarginBottom: null,
+        outerMarginLeft: null,
+        useTransformPositioning: true,
+        mobileBreakpoint: 640,
+        useBodyForBreakpoint: false,
+        minCols: 1,
+        maxCols: 100,
+        minRows: 1,
+        maxRows: 100,
+        maxItemCols: 100,
+        minItemCols: 1,
+        maxItemRows: 100,
+        minItemRows: 1,
+        maxItemArea: 2500,
+        minItemArea: 1,
+        defaultItemCols: 1,
+        defaultItemRows: 1,
+        fixedColWidth: 105,
+        fixedRowHeight: 105,
+        keepFixedHeightInMobile: false,
+        keepFixedWidthInMobile: false,
+        scrollSensitivity: 10,
+        scrollSpeed: 20,
+        enableEmptyCellClick: false,
+        enableEmptyCellContextMenu: false,
+        enableEmptyCellDrop: false,
+        enableEmptyCellDrag: false,
+        enableOccupiedCellDrop: false,
+        emptyCellDragMaxCols: 50,
+        emptyCellDragMaxRows: 50,
+        ignoreMarginInRow: false,
+        draggable: {
+          enabled: true, 
+        },
+        resizable: {
+          enabled: true
+        },
+        swap: false,
+        pushItems: true,
+        disablePushOnDrag: false,
+        disablePushOnResize: false,
+        pushDirections: { north: true, east: true, south: true, west: true },
+        pushResizeItems: false,
+        displayGrid: DisplayGrid.Always,
+        disableWindowResize: false,
+        disableWarnings: false,
+        scrollToNewItems: false
+      }
+      if (this.options!.api && this.options!.api.optionsChanged) {
+        this.options!.api.optionsChanged();
+        console.log("===>", this.options.draggable)
+      }
     }
   }
   removeItem($event: MouseEvent | TouchEvent, item:GridsterItem): void {
