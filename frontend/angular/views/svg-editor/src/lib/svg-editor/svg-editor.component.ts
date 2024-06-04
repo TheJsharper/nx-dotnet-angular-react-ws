3a01e8a07841d5e9caa82190d65a3461967a69f7
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-06-04 14:41:45 +0200

    added circle

--- frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
+++ frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule, NzSelectSizeType } from 'ng-zorro-antd/select';
+import {  NzRadioModule} from 'ng-zorro-antd/radio';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-svg-editor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzMenuModule, NzButtonModule, NzSelectModule, FormsModule, NzRadioModule],
   templateUrl: './svg-editor.component.html',
   styleUrl: './svg-editor.component.scss',
 })
 export class SvgEditorComponent {
+  selectedValue= null;
+  size: NzSelectSizeType = 'default';
+  circle:string = "";
   drawings: { x: number, y: number }[] = [
     {
       x: 10,
@@ -43,6 +51,11 @@ export class SvgEditorComponent {
 
   down($event: any, d: any): void {
     console.log("down", $event, d)
+  }
+  click($event: MouseEvent): void {
+    console.log("click", $event);
+    this.drawings.push({x:$event.offsetX  , y:   $event.offsetY});
+   // this.circle = `circle r="3" fill="red" [attr.cx]="d.x" [attr.cy]="d.y" (mousedown)="down($event, d)" (mouseup)="up()" ></circle>`;
   }
 
 }
