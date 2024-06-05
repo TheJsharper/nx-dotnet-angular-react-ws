8146d179a70f8dcc7cabc69b6235e50ba4055148
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-06-05 17:35:58 +0200

    rect angle -height and -width buggy

--- frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
+++ frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzSelectModule, NzSelectSizeType } from 'ng-zorro-antd/select';
-import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'lib-svg-editor',
@@ -15,23 +15,42 @@ import { FormsModule } from '@angular/forms';
 })
 export class SvgEditorComponent {
   selectedValue = null;
-  size: 'rect' | 'circle' | 'path' = 'circle';
+  selectItem: 'rect' | 'circle' | 'path' = 'circle';
   circle: string = "";
   circles: { x: number, y: number }[] = [];
-  rects: { x: number, y: number }[] = [];
+  rects: { x?: number, y?: number, width?: number, heigth?: number }[] = [];
+
+  rectCurrent: { x?: number, y?: number, width?: number, heigth?: number } = {};
+
   lines: { x1: number, y1: number, x2: number, y2: number }[] = [];
-  lineCurrent?: { x1?: number, y1?: number, x2?: number, y2?: number };
+  lineCurrent?: { x1?: number, y1?: number, x2?: number, y2?: number } = {};
 
   leave(): void {
     console.log("leaving")
   }
 
   move($event: MouseEvent): void {
-    if (this.lineCurrent && this.lineCurrent.x1 && this.lineCurrent.y1) {
+
+    if (this.selectItem === "path" && this.lineCurrent && this.lineCurrent.x1 && this.lineCurrent.y1) {
 
       this.lineCurrent.x2 = $event.offsetX;
       this.lineCurrent.y2 = $event.offsetY;
-      console.log("move", this.lineCurrent);
+    }
+
+    if (this.selectItem == "rect" && this.rectCurrent.x && this.rectCurrent.y) {
+      let width = $event.offsetX - this.rectCurrent.x;
+      let height = $event.offsetY - this.rectCurrent.y;
+      if (width < 0) {
+        width = -width;
+        this.rectCurrent.width = width //+ $event.offsetX ;
+      } else
+        this.rectCurrent.width = width;
+      if (height < 0) {
+        height = -height;
+        this.rectCurrent.heigth = height //+ $event.offsetY;
+      } else
+        this.rectCurrent.heigth = height;
+      console.log(width, height, height + height, $event.offsetY + this.rectCurrent.y);
     }
   }
 
@@ -43,13 +62,20 @@ export class SvgEditorComponent {
     console.log("down", $event, d)
   }
   click($event: MouseEvent): void {
-    if (this.size == 'circle') {
+    if (this.selectItem == 'circle') {
 
       this.circles.push({ x: $event.offsetX, y: $event.offsetY });
-      console.log("Click", this.size);
-    } else if (this.size == 'rect') {
-      this.rects.push({ x: $event.offsetX, y: $event.offsetY });
-      console.log("Click", this.size);
+      console.log("Click", this.selectItem);
+    } else if (this.selectItem == 'rect') {
+      if (this.rectCurrent.x && this.rectCurrent.y && this.rectCurrent.heigth && this.rectCurrent.width) {
+
+        this.rects.push({ ...this.rectCurrent });
+        this.rectCurrent = {};
+      } else {
+        this.rectCurrent = { x: $event.offsetX, y: $event.offsetY };
+      }
+
+      console.log("Click", this.selectItem);
     } else {
       if (this.lineCurrent && this.lineCurrent.x2 && this.lineCurrent.y2 && this.lineCurrent.x1 && this.lineCurrent.y1) {
 
