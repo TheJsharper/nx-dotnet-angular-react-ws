806040429aa51f909737fee71e341eeae51fe00b
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-06-05 15:31:48 +0200

    lines dynamic by drop und drop

--- frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
+++ frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
@@ -18,19 +18,21 @@ export class SvgEditorComponent {
   size: 'rect' | 'circle' | 'path' = 'circle';
   circle: string = "";
   circles: { x: number, y: number }[] = [];
-  rects: { x: number, y: number }[] = [
-    {
-      y: 150,
-      x: 100
-    }
-  ];
+  rects: { x: number, y: number }[] = [];
+  lines: { x1: number, y1: number, x2: number, y2: number }[] = [];
+  lineCurrent?: { x1?: number, y1?: number, x2?: number, y2?: number };
 
   leave(): void {
     console.log("leaving")
   }
 
-  move($event: any): void {
-    console.log("move", $event)
+  move($event: MouseEvent): void {
+    if (this.lineCurrent && this.lineCurrent.x1 && this.lineCurrent.y1) {
+
+      this.lineCurrent.x2 = $event.offsetX;
+      this.lineCurrent.y2 = $event.offsetY;
+      console.log("move", this.lineCurrent);
+    }
   }
 
   up(): void {
@@ -41,7 +43,6 @@ export class SvgEditorComponent {
     console.log("down", $event, d)
   }
   click($event: MouseEvent): void {
-    //  console.log("click", $event);
     if (this.size == 'circle') {
 
       this.circles.push({ x: $event.offsetX, y: $event.offsetY });
@@ -49,9 +50,13 @@ export class SvgEditorComponent {
     } else if (this.size == 'rect') {
       this.rects.push({ x: $event.offsetX, y: $event.offsetY });
       console.log("Click", this.size);
-    }else{
-      
-      this.rects.push({ x: $event.offsetX, y: $event.offsetY });
+    } else {
+      if (this.lineCurrent && this.lineCurrent.x2 && this.lineCurrent.y2 && this.lineCurrent.x1 && this.lineCurrent.y1) {
+
+        this.lines.push({ x1: this.lineCurrent.x1, y1: this.lineCurrent.y1, x2: this.lineCurrent.x2, y2: this.lineCurrent.y2 });
+        this.lineCurrent = {};
+      } else
+        this.lineCurrent = { x1: $event.offsetX, y1: $event.offsetY }
     }
   }
 
