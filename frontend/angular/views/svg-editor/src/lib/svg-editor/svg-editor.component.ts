34e6b65e1cb9b95e706570ff15a553a92f7e31ed
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-06-05 12:04:37 +0200

    added last committing

--- frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
+++ frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzSelectModule, NzSelectSizeType } from 'ng-zorro-antd/select';
-import {  NzRadioModule} from 'ng-zorro-antd/radio';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -14,11 +14,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './svg-editor.component.scss',
 })
 export class SvgEditorComponent {
-  selectedValue= null;
-  size: NzSelectSizeType = 'default';
-  circle:string = "";
-  drawings: { x: number, y: number }[] = [
-  ]
+  selectedValue = null;
+  size: 'rect' | 'circle' | 'path' = 'circle';
+  circle: string = "";
+  circles: { x: number, y: number }[] = [];
+  rects: { x: number, y: number }[] = [
+    {
+      y: 150,
+      x: 100
+    }
+  ];
 
   leave(): void {
     console.log("leaving")
@@ -36,8 +41,18 @@ export class SvgEditorComponent {
     console.log("down", $event, d)
   }
   click($event: MouseEvent): void {
-    console.log("click", $event);
-    this.drawings.push({x:$event.offsetX  , y:   $event.offsetY});
+    //  console.log("click", $event);
+    if (this.size == 'circle') {
+
+      this.circles.push({ x: $event.offsetX, y: $event.offsetY });
+      console.log("Click", this.size);
+    } else if (this.size == 'rect') {
+      this.rects.push({ x: $event.offsetX, y: $event.offsetY });
+      console.log("Click", this.size);
+    }else{
+      
+      this.rects.push({ x: $event.offsetX, y: $event.offsetY });
+    }
   }
 
 }
