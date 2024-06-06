d56a43bd2bd575699ea1887b39110d2346e3a8cb
Pedro Salomon <pedro.salomonocampos@eviden.com>
2024-06-06 17:38:23 +0200

    lastest changing

--- frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
+++ frontend/angular/views/svg-editor/src/lib/svg-editor/svg-editor.component.ts
@@ -5,12 +5,11 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-//import { DataTableComponent} from 'angular-components-core-ui-0.0.1-124147/package'
 
 @Component({
   selector: 'lib-svg-editor',
   standalone: true,
-  imports: [CommonModule, NzMenuModule, NzButtonModule, NzSelectModule, FormsModule, NzRadioModule/* , DataTableComponent */],
+  imports: [CommonModule, NzMenuModule, NzButtonModule, NzSelectModule, FormsModule, NzRadioModule],
   templateUrl: './svg-editor.component.html',
   styleUrl: './svg-editor.component.scss',
 })
@@ -27,7 +26,7 @@ export class SvgEditorComponent {
   lineCurrent?: { x1?: number, y1?: number, x2?: number, y2?: number } = {};
 
   leave(): void {
-    if(this.selectItem === "rect" && this.rectCurrent.x && this.rectCurrent.y && this.rectCurrent.width && this.rectCurrent.heigth){
+    if (this.selectItem === "rect" && this.rectCurrent.x && this.rectCurrent.y && this.rectCurrent.width && this.rectCurrent.heigth) {
       this.rects.push({ ...this.rectCurrent });
       this.rectCurrent = {};
     }
@@ -35,7 +34,7 @@ export class SvgEditorComponent {
   }
 
   move($event: MouseEvent): void {
-    const e  = $event;
+    const e = $event;
     if (this.selectItem === "path" && this.lineCurrent && this.lineCurrent.x1 && this.lineCurrent.y1) {
 
       this.lineCurrent.x2 = $event.offsetX;
@@ -45,23 +44,21 @@ export class SvgEditorComponent {
     if (this.selectItem == "rect" && this.rectCurrent.x && this.rectCurrent.y) {
       const width = Math.abs($event.offsetX - this.rectCurrent.x);
       const height = Math.abs($event.offsetY - this.rectCurrent.y);
-   
-      if($event.offsetX - this.rectCurrent.x < 0){
-          this.rectCurrent.width =   Math.max($event.offsetX , this.rectCurrent.x)- $event.offsetX;
-      }else{
+
+      if ($event.offsetX - this.rectCurrent.x < 0) {
+        this.rectCurrent.width = Math.max($event.offsetX, this.rectCurrent.x) - $event.offsetX;
+      } else {
 
         this.rectCurrent.width = width;
       }
 
-      if($event.offsetY - this.rectCurrent.y < 0){
-        this.rectCurrent.heigth = Math.max($event.offsetY , this.rectCurrent.y) -$event.offsetY ;
-    }else{
+      if ($event.offsetY - this.rectCurrent.y < 0) {
+        this.rectCurrent.heigth = Math.max($event.offsetY, this.rectCurrent.y) - $event.offsetY;
+      } else {
 
-      this.rectCurrent.heigth = height;
-    }
-  
-      //console.log(this.rectCurrent.x, this.rectCurrent.y, width, height, "==> ",  this.rectCurrent.x - $event.offsetY, "==>x ", this.rectCurrent.x - this.rectCurrent.y  );
-     // console.log("=>x", e.)
+        this.rectCurrent.heigth = height;
+      }
+
     }
   }
 
