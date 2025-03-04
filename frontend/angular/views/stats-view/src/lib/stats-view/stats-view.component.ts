2796aade744308d5b3732750d17028bb5c7f2633
Pedro Salomon <pedro.salomon@gmail.com>
2025-03-04 22:00:02 +0100

    StatView use two libraries

--- frontend/angular/views/stats-view/src/lib/stats-view/stats-view.component.ts
+++ frontend/angular/views/stats-view/src/lib/stats-view/stats-view.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterStickyTableComponent } from 'footer-sticky-table';
 import { PlotlyViewComponent } from 'plotly-view';
 @Component({
   selector: 'lib-stats-view',
   standalone: true,
-  imports: [CommonModule,  RouterOutlet, PlotlyViewComponent],
+  imports: [CommonModule,   PlotlyViewComponent, FooterStickyTableComponent],
   templateUrl: './stats-view.component.html',
   styleUrl: './stats-view.component.scss',
 })
