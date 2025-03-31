2bb89ceef59b7716ad09da38b2d6741f45114731
Pedro Salomon <pedro.salomon@gmail.com>
2025-03-31 22:01:34 +0200

    todo microapp

--- frontend/angular/micro-apps/mf-todo/src/lib/todo-component.ts
+++ frontend/angular/micro-apps/mf-todo/src/lib/todo-component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'lib-todo-main',
     template: '<router-outlet/>',
+    styleUrls: ['./todo-component.css'],
     imports:[RouterOutlet]
 })
 
