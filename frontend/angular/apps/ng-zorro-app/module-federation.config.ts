36306efebc5bf901a2a815a7a4b9c355407d8069
Pedro Salomon <pedro.salomon@gmail.com>
2025-04-04 21:52:35 +0200

    conveting ng-zorro-app host application

---
+++ frontend/angular/apps/ng-zorro-app/module-federation.config.ts
@@ -0,0 +1,42 @@
+import { ModuleFederationConfig } from '@nx/module-federation';
+
+const coreLibraries = new Set([
+  'bootstrap',
+  'bootstrap-icons'
+]);
+
+
+const config/**/: ModuleFederationConfig = {
+  name: 'ng-zorro-app',
+  /**
+   * To use a remote that does not exist in your current Nx Workspace
+   * You can use the tuple-syntax to define your remote
+   *
+   * remotes: [['my-external-remote', 'https://nx-angular-remote.netlify.app']]
+   *
+   * You _may_ need to add a `remotes.d.ts` file to your `src/` folder declaring the external remote for tsc, with the
+   * following content:
+   *
+   * declare module 'my-external-remote';
+   *
+   */
+  shared: (libraryName, defaultConfig) => {
+    console.log("RUNTIME Product  ===>", libraryName, defaultConfig)
+    if (coreLibraries.has(libraryName)) {
+      return defaultConfig;
+    }
+
+    // Returning false means the library is not shared.
+    return defaultConfig;
+  },
+  additionalShared: [
+    {
+      libraryName: '@nx/angular/mf',
+      sharedConfig: { eager:false}, // also worked with your settings;
+      
+    }
+  ],
+  remotes: [],
+};
+
+export default config;
