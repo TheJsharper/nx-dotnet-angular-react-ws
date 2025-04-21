ea613768651d6efc2b8e630cd71294dfe8b92126
Pedro Salomon <pedro.salomon@gmail.com>
2025-04-21 23:27:33 +0200

    fixed problem with store and sharing laibraries

--- frontend/angular/apps/ng-zorro-app/module-federation.config.ts
+++ frontend/angular/apps/ng-zorro-app/module-federation.config.ts
@@ -2,6 +2,20 @@ import { ModuleFederationConfig } from '@nx/module-federation';
 
 const coreLibraries = new Set([
   'bootstrap',
+  "@angular/animations",
+  "@angular/common",
+  "@angular/compiler",
+  "@angular/core",
+  "@angular/forms",
+  "@angular/material",
+  "@angular/platform-browser",
+  "@angular/platform-browser-dynamic",
+  "@angular/router",
+
+  "@ngrx/effects",
+  "@ngrx/store",
+  "@ngrx/store-devtools",
+  "rxjs",
 ]);
 const config: ModuleFederationConfig = {
   name: 'ng-zorro-app',
@@ -27,7 +41,7 @@ const config: ModuleFederationConfig = {
     //console.log(`Shared library: =======>x${libraryName}`);
 
     // Returning false means the library is not shared.
-    return defaultConfig;
+    return false;
   },
   additionalShared: ['bootstrap'],
   disableNxRuntimeLibraryControlPlugin: false,
