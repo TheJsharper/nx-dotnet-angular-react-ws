6a676fdaa92768f2ee052cd83cb0e2180675672d
Pedro Salomon <pedro.salomon@gmail.com>
2025-05-20 21:49:01 +0200

    added mf host react app

---
+++ frontend/react/micro-frontends/hosts/about-hooks/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'about-hooks',
+  preset: '../../../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../../coverage/frontend/react/micro-frontends/hosts/about-hooks',
+};
