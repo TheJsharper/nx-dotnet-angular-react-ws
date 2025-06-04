2760046dfa034559c7516bf5bdf7f1aff11f6120
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-04 17:30:00 +0200

    styking CounterUpdatePrevious component

--- frontend/react/micro-frontends/remotes/use-state-app/src/app/components/CounterUpdateBasedPrevious.tsx
+++ frontend/react/micro-frontends/remotes/use-state-app/src/app/components/CounterUpdateBasedPrevious.tsx
@@ -7,12 +7,12 @@ const Example = () => {
     const initialCount = 0;
 
     const initialHistoryIncrement = [initialCount];
+
     const initialHistoryDecrement = [initialCount];
 
     const [currentHistoryIncrement, setCurrentHistoryIncrement] = useState(initialHistoryIncrement);
 
     const [currentHistoryDecrement, setCurrentHistoryDecrement] = useState(initialHistoryDecrement);
-
 
     const [count, setCount] = useState(initialCount);
 
@@ -37,29 +37,33 @@ const Example = () => {
 
 
     return (
-        <div>
-            <h1>Counter Update Based on Previous Value</h1>
-            <span>Current Count: {count}</span>
+        <div className="container mt-4 ">
+            <div className="d-flex flex-column ">
+                <h1 className="justify-content-center">Counter Update Based on Previous Value</h1>
+                <div className="d-flex item-center justify-content-center">
+                    <span className="badge bg-primary ">Current Count: {count}</span>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-success"> History Increment </span> </h5>
+                    <div>
+                        {currentHistoryIncrement.map((value, index) => (
+                            <span className="badge bg-success me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-danger"> History Decrement </span> </h5>
+                    <div>
+                        {currentHistoryDecrement.map((value, index) => (
 
-            <div className="d-flex">
-                <h5> <span className="badge bg-success"> History Increment </span> </h5>
-
-                {currentHistoryIncrement.map((value, index) => (
-                    <span className="badge bg-success" key={index}> {`[${index + 1}`}: {`${value}]`}</span>
-                ))}
-            </div>
-            <div className="d-flex">
-                <h5> <span className="badge bg-danger"> History Decrement </span> </h5>
-                {currentHistoryDecrement.map((value, index) => (
-                    <span className="badge bg-danger" key={index}> {`[${index + 1}`}: {`${value}]`}</span>
-                ))}
-            </div>
-
-
-
-            <div className="d-flex">
-                <button className="btn btn-success" onClick={incrementCount}>Increment</button>
-                <button className="btn btn-danger" onClick={decrementCount}>Decrement</button>
+                            <span className="badge bg-danger me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex   justify-content-between mt-4">
+                    <button className="btn btn-success" onClick={incrementCount}>Increment</button>
+                    <button className="btn btn-danger" onClick={decrementCount}>Decrement</button>
+                </div>
             </div>
         </div>
     );
@@ -72,78 +76,148 @@ const CounterUpdateBasedPrevious = () => {
 import { Dispatch, SetStateAction, useState } from "react";
 
 const Example = () => {
-    const [count, setCount]: [number, Dispatch<SetStateAction<number>>] = useState<number>(0);
+    const initialCount = 0;
 
-    const handlerClickIncr = () => {
-        setCount(count + 1);
+    const initialHistoryIncrement = [initialCount];
 
+    const initialHistoryDecrement = [initialCount];
+
+    const [currentHistoryIncrement, setCurrentHistoryIncrement] = useState(initialHistoryIncrement);
+
+    const [currentHistoryDecrement, setCurrentHistoryDecrement] = useState(initialHistoryDecrement);
+
+    const [count, setCount] = useState(initialCount);
+
+
+    const incrementCount = () => {
+        setCount(prevCount => {
+            const newCount = prevCount + 1;
+            setCurrentHistoryIncrement(prevHistory => [...prevHistory, newCount]);
+            setCurrentHistoryDecrement(prevHistory => [...prevHistory, newCount]);
+            return newCount
+        });
     }
-    const handlerClickDecr = () => {
-        setCount(count - 1);    
+
+    const decrementCount = () => {
+        setCount(prevCount => {
+            const newCount = prevCount - 1;
+            setCurrentHistoryIncrement(prevHistory => [...prevHistory, newCount]);
+            setCurrentHistoryDecrement(prevHistory => [...prevHistory, newCount]);
+            return newCount
+        });
     }
+
 
     return (
-        <div>
-            <h1>Counter: {count}</h1>
-            <button onClick={handlerClickIncr}>Increment</button>
-            <button onClick={handlerClickDecr}>Decrement</button>
+        <div className="container mt-4 ">
+            <div className="d-flex flex-column ">
+                <h1 className="justify-content-center">Counter Update Based on Previous Value</h1>
+                <div className="d-flex item-center justify-content-center">
+                    <span className="badge bg-primary ">Current Count: {count}</span>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-success"> History Increment </span> </h5>
+                    <div>
+                        {currentHistoryIncrement.map((value, index) => (
+                            <span className="badge bg-success me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-danger"> History Decrement </span> </h5>
+                    <div>
+                        {currentHistoryDecrement.map((value, index) => (
+
+                            <span className="badge bg-danger me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex   justify-content-between mt-4">
+                    <button className="btn btn-success" onClick={incrementCount}>Increment</button>
+                    <button className="btn btn-danger" onClick={decrementCount}>Decrement</button>
+                </div>
+            </div>
         </div>
     );
 }
 
 
 `;
-    const styling = `
-    const Example = () => {
-
+const styling = `
+ const Example = () => {
     return (
-        <div>
-            <h1>Counter: {1}</h1>
-            <button className="btn btn-primary" >Increment</button>
-            <button className="btn btn-danger">Decrement</button>
+        <div className="container mt-4 ">
+            <div className="d-flex flex-column ">
+                <h1 className="justify-content-center">Counter Update Based on Previous Value</h1>
+                <div className="d-flex item-center justify-content-center">
+                    <span className="badge bg-primary ">Current Count: {1}</span>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-success"> History Increment </span> </h5>
+                    <div>
+                        {currentHistoryIncrement.map((value, index) => (
+                            <span className="badge bg-success me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex flex-column mt-4">
+                    <h5> <span className="badge bg-danger"> History Decrement </span> </h5>
+                    <div>
+                        {currentHistoryDecrement.map((value, index) => (
+
+                            <span className="badge bg-danger me-2" key={index}> {"[" + index + 1}:{value + "]"}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="d-flex   justify-content-between mt-4">
+                    <button className="btn btn-success" >Increment</button>
+                    <button className="btn btn-danger" >Decrement</button>
+                </div>
+            </div>
         </div>
     );
 }
 
+
 `;
 
-    const [language,] = useState("tsx");
-    const [languageDemo,] = useState(code);
-    const [lineNumbers] = useState(true);
-    return (
-        <div className="container">
-            <Container className="mt-4">
-                <Row>
-                    <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
-                        <Tab eventKey="home" title="Looking at UseState">
-                            <Example />
-                        </Tab>
-                        <Tab eventKey="code" title="Visual Code">
-                            <h1> Example Visual Code</h1>
-                            <CopyBlock
-                                language={language}
-                                text={languageDemo}
-                                showLineNumbers={lineNumbers}
-                                theme={dracula}
-                                codeBlock
-                            />
-                        </Tab>
-                        <Tab eventKey="styling" title="styling">
-                            <h1>Styling</h1>
-                            <h1> Example Visual Code</h1>
-                            <CopyBlock
-                                language={'css'}
-                                text={styling}
-                                showLineNumbers={lineNumbers}
-                                theme={dracula}
-                                codeBlock
-                            />
-                        </Tab>
-                    </Tabs>
-                </Row>
-            </Container>
-        </div>
-    );
+const [language,] = useState("tsx");
+const [languageDemo,] = useState(code);
+const [lineNumbers] = useState(true);
+return (
+    <div className="container">
+        <Container className="mt-4">
+            <Row>
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3">
+                    <Tab eventKey="home" title="Looking at UseState">
+                        <Example />
+                    </Tab>
+                    <Tab eventKey="code" title="Visual Code">
+                        <h1> Example Visual Code</h1>
+                        <CopyBlock
+                            language={language}
+                            text={languageDemo}
+                            showLineNumbers={lineNumbers}
+                            theme={dracula}
+                            codeBlock
+                        />
+                    </Tab>
+                    <Tab eventKey="styling" title="styling">
+                        <h1>Styling</h1>
+                        <h1> Example Visual Code</h1>
+                        <CopyBlock
+                            language={'css'}
+                            text={styling}
+                            showLineNumbers={lineNumbers}
+                            theme={dracula}
+                            codeBlock
+                        />
+                    </Tab>
+                </Tabs>
+            </Row>
+        </Container>
+    </div>
+);
 }
 
 export default CounterUpdateBasedPrevious;
