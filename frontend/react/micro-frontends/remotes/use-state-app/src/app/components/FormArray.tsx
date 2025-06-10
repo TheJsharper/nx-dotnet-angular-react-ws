bac9c24a84aa7bd39475b2b140ce62d5687c7245
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-10 21:45:50 +0200

    styling FormArray Component

--- frontend/react/micro-frontends/remotes/use-state-app/src/app/components/FormArray.tsx
+++ frontend/react/micro-frontends/remotes/use-state-app/src/app/components/FormArray.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from "react";
+import { useState } from "react";
 import { Container, Row, Tab, Tabs } from "react-bootstrap";
 import { CopyBlock, dracula } from "react-code-blocks";
 
@@ -20,14 +20,14 @@ const AddTodo = ({ onAddTodo }: { onAddTodo: (title: string) => void }) => {
     };
 
     return (
-        <div>
-            <input
+        <div className="d-flex flex-row align-items-center justify-content-between gap-2">
+            <input className="form-control"
                 type="text"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 placeholder="Add a new todo"
             />
-            <button onClick={handleAddTodo}>Add </button>
+            <button className="btn btn-info" onClick={handleAddTodo}>Add </button>
         </div>
     );
 }
@@ -35,49 +35,50 @@ const AddTodo = ({ onAddTodo }: { onAddTodo: (title: string) => void }) => {
 const Task = ({ todo, onChange, OnDelete }: { todo: Todo, onChange: (todo: Todo) => void, OnDelete: (id: number) => void }) => {
 
     const [isEditing, setIsEditing] = useState(false);
+ 
+    const getContent: () => JSX.Element = () => {
 
+        if (isEditing) {
+            return (
+                <>
+                    <input className="form-control"
+                        value={todo.title}
+                        onChange={(e) => onChange({ ...todo, title: e.target.value })}
 
-    let todoContent:ReactElement = (<span>Loading...</span>)
+                    />
 
-    if (isEditing) {
-        todoContent = (
-            <>
-                <input
-                    value={todo.title}
-                    onChange={(e) => onChange({ ...todo, title: e.target.value })}
+                    <button className="btn btn-success" onClick={() => setIsEditing(false)}>Save</button>
+                </>
+            );
+        } else {
+            return (
+                <>
+                    <label >
+                        {todo.title} </label>
+                    <button className="btn btn-warning" onClick={() => setIsEditing(true)}>Edit</button>
 
-                />
-
-                <button onClick={() => setIsEditing(false)}>Save</button>
-            </>
-        );
-    } else {
-        todoContent = (
-            <>
-                {todo.title}
-                <button onClick={() => setIsEditing(true)}>Edit</button>
-
-            </>
-        );
+                </>
+            );
+        }
     }
     return (
-        <>
-            <input
+        < div className="d-flex flex-row align-items-center justify-content-between gap-2" >
+            <input className="form-check-input"
                 type="checkbox"
                 checked={todo.done}
                 onChange={(e) => onChange({ ...todo, done: e.target.checked })}
             />
-            {todoContent}
-            <button onClick={() => OnDelete(todo.id)}>Delete</button>
-        </>
+            {getContent()}
+            <button className="btn btn-danger" onClick={() => OnDelete(todo.id)}>Delete</button>
+        </div>
     );
 }
 
 const TaskList = ({ todos, onChange, OnDelete }: { todos: Todo[], onChange: (task: Todo) => void, OnDelete: (id: number) => void }) => {
-    return (
-        <ul>
+    return (<div className="d-flex  flex-row gap-5"   >
+        <ul className="list-group">
             {todos.map(todo => (
-                <li key={todo.id}>
+                <li className="list-group-item" key={todo.id}>
                     <Task
                         todo={todo}
                         onChange={onChange}
@@ -87,6 +88,8 @@ const TaskList = ({ todos, onChange, OnDelete }: { todos: Todo[], onChange: (tas
                 </li>
             ))}
         </ul>
+
+    </div>
     );
 }
 
@@ -123,12 +126,18 @@ const Example = () => {
 
 
     return (
-        <>
-            <AddTodo onAddTodo={handleAddTodo} />
-            <TaskList todos={todos} onChange={handleChangeTodo} OnDelete={handleDeleteTodo} />
+        < div className="d-flex gap-5">
+            <div className="d-flex flex-column gap-2">
+                <AddTodo onAddTodo={handleAddTodo}   />
+                <TaskList todos={todos} onChange={handleChangeTodo} OnDelete={handleDeleteTodo} />
+            </div>
 
-
-        </>
+            <pre className="flex-fill">
+                <code style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}           >
+                    {JSON.stringify(todos, null, 2)}
+                </code>
+            </pre>
+        </div>
     );
 }
 
