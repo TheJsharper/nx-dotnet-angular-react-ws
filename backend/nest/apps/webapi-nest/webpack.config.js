6daa0e1eaa6f35bc89a4e96c8316ac9af9049ba1
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-13 16:47:37 +0200

    recovered nest projects

--- backend/nest/apps/webapi-nest/webpack.config.js
+++ backend/nest/apps/webapi-nest/webpack.config.js
@@ -1,8 +1,25 @@
-import { composePlugins, withNx } from '@nx/webpack';
 
-// Nx plugins for webpack.
-export default composePlugins(withNx(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
-  return config;
-});
+import { NxAppWebpackPlugin } from '@nx/webpack/app-plugin.js';
+import { join } from 'path';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+export const output = {
+  path: join(__dirname, '../../../../dist/backend/nest/apps/webapi-nest'),
+};
+export const plugins = [
+  new NxAppWebpackPlugin({
+    target: 'node',
+    compiler: 'tsc',
+    main: './src/main.ts',
+    tsConfig: './tsconfig.app.json',
+    assets: ['./src/assets'],
+    optimization: false,
+    outputHashing: 'none',
+    generatePackageJson: true,
+  }),
+];
