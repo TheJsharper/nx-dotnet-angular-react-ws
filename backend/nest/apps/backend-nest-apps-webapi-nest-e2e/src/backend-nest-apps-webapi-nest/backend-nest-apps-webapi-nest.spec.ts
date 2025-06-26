fe57de38ef8e0cf2a9269ffff3e1f6b5cd82a4e1
Pedro Salomon <pedro.salomon@gmail.com>
2025-06-26 22:49:57 +0200

    fixed tests

--- backend/nest/apps/backend-nest-apps-webapi-nest-e2e/src/backend-nest-apps-webapi-nest/backend-nest-apps-webapi-nest.spec.ts
+++ backend/nest/apps/backend-nest-apps-webapi-nest-e2e/src/backend-nest-apps-webapi-nest/backend-nest-apps-webapi-nest.spec.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 describe('GET /api', () => {
   it('should return a message', async () => {
-    const res = await axios.get(`/api/message`);
+    const res = await axios.get(`/api/app/message`);
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
   });
   it('should return list of cars', async () => {
-    const res = await axios.get(`/api/cars`);
+    const res = await axios.get(`/api/app/cars`);
 
     expect(res.status).toBe(200);
     
