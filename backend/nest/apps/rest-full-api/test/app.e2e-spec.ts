eb2cf575304415e99ac3083bf9de48647a672d19
Pedro Salomon <pedro.salomon@gmail.com>
2025-07-09 16:01:59 +0200

    rest-full-api project converted to nx project

--- backend/nest/apps/rest-full-api/test/app.e2e-spec.ts
+++ backend/nest/apps/rest-full-api/test/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/*import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
@@ -22,3 +22,4 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
+*/
---
+++ backend/nest/apps/rest-full-api/webpack.config.js
@@ -0,0 +1,26 @@
+
+import { NxAppWebpackPlugin } from '@nx/webpack/app-plugin.js';
+import { join } from 'path';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+export const output = {
+  path: join(__dirname, '../../../../dist/backend/nest/apps/rest-full-api'),
+};
+export const plugins = [
+  new NxAppWebpackPlugin({
+    target: 'node',
+    compiler: 'tsc',
+    main: './src/main.ts',
+    tsConfig: './tsconfig.app.json',
+    assets: ['./src/assets'],
+    optimization: false,
+    outputHashing: 'none',
+    generatePackageJson: true,
+    
+  }),
+];
